b5638233b95c783af92d07ca9799cda6bb6fc69d
ducnguyen <minhducnguyen189@gmail.com>
2022-11-27 20:42:32 +0700

    update init table

--- flyway-app/src/main/resources/db/migration/V1.0/V1.0_20220426223800__Initial_Table.sql
+++ flyway-app/src/main/resources/db/migration/V1.0/V1.0_20220426223800__Initial_Table.sql
@@ -3,7 +3,7 @@ CREATE TABLE `customers` (
   `address` varchar(255) DEFAULT NULL,
   `dob` datetime(6) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `fullName` varchar(255) DEFAULT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
